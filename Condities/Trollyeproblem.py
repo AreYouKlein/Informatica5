2083e65d2fa2a577a1ea9d20667b56f0491579ae
AreYouKlein <42997604+AreYouKlein@users.noreply.github.com>
2018-10-03 11:58:05 +0200

    Verkeerde naam :\

---
+++ Condities/Trollyeproblem.py
@@ -0,0 +1,9 @@
+hendel = input('Trek je aan de wisselhendel? :')
+duwen = input('Duw je de man van de brug? :')
+
+if hendel == 'ja' and duwen == 'ja':
+    print(2)
+elif hendel == 'nee' and duwen == 'nee':
+    print(5)
+else:
+    print(1)
